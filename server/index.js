1d835ac7d0f3388b67b3c36687c2bcb3345422f4
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-05 15:10:55 +0700

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
+dotenv.config();
+
+const app = express();
+const PROT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
+app.listen(PROT, () => {
   console.log("Server running on port 3000");
 });
